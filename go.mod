cb54f4c49a15f4ea27b801cbcbb0062fc12a8db7
Rekunov Dmitriy <drekunov@inertech.ru>
2022-08-23 12:06:11 +0300

    Add atack zip function

--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module bzip
 
 go 1.18
 
-require (
-	github.com/alexmullins/zip v0.0.0-20180717182244-4affb64b04d0
-	github.com/yeka/zip v0.0.0-20180914125537-d046722c6feb
-)
+require github.com/yeka/zip v0.0.0-20180914125537-d046722c6feb
 
 require golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
--- pkg/atack/atack_zip.go
+++ pkg/atack/atack_zip.go
@@ -1,39 +1,50 @@
 package atack
 
 import (
-	"archive/zip"
 	"bytes"
-	"github.com/alexmullins/zip"
-	"io"
+	zzz "github.com/yeka/zip"
 	"log"
 	"os"
 )
 
 type azip struct {
 	fileName string
+	content  []byte
+	size     int64
 }
 
 func New(fileName string) *azip {
-	contents := []byte("Hello World")
-	fzip, err := os.Create(`./test.zip`)
+	var err error
+	z := &azip{fileName: fileName}
+
+	z.content, err = os.ReadFile(fileName)
 	if err != nil {
-		log.Fatalln(err)
+		panic("Error open file!!!")
+		return nil
 	}
-	zipw := zip.NewWriter(fzip)
-	defer zipw.Close()
-	w, err := zipw.Encrypt(`test.txt`, `golang`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = io.Copy(w, bytes.NewReader(contents))
-	if err != nil {
-		log.Fatal(err)
-	}
-	zipw.Flush()
-	return &azip{fileName: fileName}
+	z.size = int64(len(z.content))
+	return z
 }
 
-func (a azip) check(pass string) bool {
-	//TODO implement me
-	panic("implement me")
+func (a *azip) check(pass string) bool {
+	r, err := zzz.NewReader(bytes.NewReader(a.content), a.size)
+	if err != nil {
+		return false
+	}
+
+	for _, f := range r.File {
+		if f.IsEncrypted() {
+			f.SetPassword(pass)
+		}
+
+		r, err := f.Open()
+		if err != nil {
+			log.Println(err)
+			return false
+		} else {
+			r.Close()
+			return true
+		}
+	}
+	return false
 }
